fbd66bc3c3ee84de3579bb009823173a1e071af7
melikaabedikoupaei <118809158+melikaabedikoupaei@users.noreply.github.com>
2024-07-30 15:34:34 +0200

    clean url

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -8,6 +8,3 @@ def Data_visualization_project (request):
     return render (request,"Data_visualization_project.html") 
 def roadmap(request):
     return render (request,"roadmap.html") 
-def proxy_audio(request):
-    response = requests.get('https://drive.google.com/uc?export=download&id=1YvuC0Ivn5QCq0h8sAdOoyFi11mnhwprz')
-    return HttpResponse(response.content, content_type='audio/mpeg')
