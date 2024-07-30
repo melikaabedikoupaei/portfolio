b36a4827b92848369dcadb47f1db745fa3b2e826
melikaabedikoupaei <118809158+melikaabedikoupaei@users.noreply.github.com>
2024-07-30 15:26:03 +0200

    add audio in main page

--- Portfolio/views.py
+++ Portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 def index (request):
     return render (request,"index.html") 
@@ -7,3 +8,6 @@ def Data_visualization_project (request):
     return render (request,"Data_visualization_project.html") 
 def roadmap(request):
     return render (request,"roadmap.html") 
+def proxy_audio(request):
+    response = requests.get('https://drive.google.com/uc?export=download&id=1YvuC0Ivn5QCq0h8sAdOoyFi11mnhwprz')
+    return HttpResponse(response.content, content_type='audio/mpeg')
